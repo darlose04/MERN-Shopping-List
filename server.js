9de8ce97f73e77d85120f9ae9db5d1dc10b48320
Zach Smith <zsmith126028@gmail.com>
2019-05-30 17:20:36 -0500

    connect to mongo

--- server.js
+++ server.js
@@ -7,3 +7,10 @@ const app = express();
 // Bodyparser Middleware
 app.use(bodyParser.json());
 
+// DB Config
+const db = require('./config/keys').MongoURI;
+
+// Connect to Mongo
+mongoose.connect(db, { useNewUrlParser: true, dbName: 'shoppinglist' })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
