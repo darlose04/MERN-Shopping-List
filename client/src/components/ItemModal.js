92601a815f882cd9cc6c256a3dfdd0e70e36caa2
Zach Smith <zsmith126028@gmail.com>
2019-05-31 18:38:55 -0500

    begin work on separate component form for adding items

---
+++ client/src/components/ItemModal.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Form,
+  FormGroup,
+  Label,
+  Input
+} from 'reactstrap';
+import { connect } from 'react-redux';
+import { addItem } from '../actions/itemActions';
+
+class ItemModal extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default ItemModal;
