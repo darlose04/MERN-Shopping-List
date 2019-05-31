eaead5a9f7db6491e20706b4bcb8cf463b939caf
Zach Smith <zsmith126028@gmail.com>
2019-05-31 17:56:05 -0500

    add actions file for item types

---
+++ client/src/actions/itemActions.js
@@ -0,0 +1,8 @@
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from './types';
+
+export const getItems = () => {
+  // this return is going to the itemReducer and checking the action.type
+  return {
+    type: GET_ITEMS
+  }
+}
