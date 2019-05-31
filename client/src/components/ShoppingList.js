66a1eff0e30558d17fa293feec8883d22c7a5a53
Zach Smith <zsmith126028@gmail.com>
2019-05-30 22:49:50 -0500

    setup initial shoppinglist component

---
+++ client/src/components/ShoppingList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import uuid from 'uuid';
+
+class ShoppingList extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default ShoppingList;
