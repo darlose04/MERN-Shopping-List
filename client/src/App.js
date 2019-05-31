a2e7f919d949f4065339081258606db1c2cfe6b4
Zach Smith <zsmith126028@gmail.com>
2019-05-31 17:39:27 -0500

    add Provider and store to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -8,10 +11,12 @@ import ShoppingList from './components/ShoppingList';
 
 function App() {
   return (
-    <div className="App">
-      <AppNavbar />
-      <ShoppingList />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <AppNavbar />
+        <ShoppingList />
+      </div>
+    </Provider>
   );
 }
 
