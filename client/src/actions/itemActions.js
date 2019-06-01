85c3aa28aef3a5d414e3ae9c3c71342560c203be
Zach Smith <zsmith126028@gmail.com>
2019-05-31 23:26:15 -0500

    add setItemsLoading

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,4 +1,4 @@
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from './types';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from './types';
 
 export const getItems = () => {
   // this return is going to the itemReducer and checking the action.type
@@ -18,5 +18,11 @@ export const addItem = (item) => {
   return {
     type: ADD_ITEM,
     payload: item
+  }
+}
+
+export const setItemsLoading = () => {
+  return {
+    type: ITEMS_LOADING
   }
 }
