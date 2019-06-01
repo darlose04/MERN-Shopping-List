b3d8acfb1861ab87bbcf43a650959fc9b62fbec1
Zach Smith <zsmith126028@gmail.com>
2019-05-31 23:37:29 -0500

    connect to backend in order to retrieve new items added to db

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -13,20 +13,28 @@ export const getItems = () => dispatch => {
     .then(res => dispatch({
       type: GET_ITEMS,
       payload: res.data
-    }))
+    }));
+}
+
+export const addItem = (item) => dispatch => {
+  // return {
+  //   type: ADD_ITEM,
+  //   payload: item
+  // }
+
+  // returns new item that is from items.js in backend routes/api
+  axios
+    .post('/api/items', item)
+    .then(res => dispatch({
+      type: ADD_ITEM,
+      payload: res.data
+    }));
 }
 
 export const deleteItem = (id) => {
   return {
     type: DELETE_ITEM,
     payload: id
-  }
-}
-
-export const addItem = (item) => {
-  return {
-    type: ADD_ITEM,
-    payload: item
   }
 }
 
