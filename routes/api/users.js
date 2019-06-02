bd2b9f43b798bd8a18e1576549b4cf182776e3c6
Zach Smith <zsmith126028@gmail.com>
2019-06-01 21:49:36 -0500

    add validation, check for existing user, create new user and hash password

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
 // User Model
 const User = require('../../models/User');
@@ -8,7 +9,44 @@ const User = require('../../models/User');
 // @description Register new user
 // @access Public
 router.post('/', (req, res) => {
-  res.send('register');
+  const { name, email, password } = req.body;
+
+  // simple validation
+  if (!name || !email || !password) {
+    return res.status(400).json({ msg: 'Please enter all fields' });
+  }
+
+  // check for existing user
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+
+      const newUser = new User({
+        name,
+        email,
+        password
+      });
+
+      // Create salt and hash 
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser.save()
+            .then(user => {
+              res.json({
+                user: {
+                  id: user.id,
+                  name: user.name,
+                  email: user.email
+                }
+              })
+            });
+        });
+      });
+    })
 });
 
 module.exports = router;
