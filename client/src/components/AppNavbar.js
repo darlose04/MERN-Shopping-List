f18f09228304bd3dcbeaf9ef4f7530e626f962f6
Zach Smith <zsmith126028@gmail.com>
2019-06-04 15:26:56 -0500

    bring in RegisterModal

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   Collapse,
   Navbar,
@@ -8,37 +8,36 @@ import {
   NavItem,
   NavLink,
   Container
-} from 'reactstrap';
+} from "reactstrap";
+import RegisterModal from "./auth/RegisterModal";
 
 class AppNavbar extends Component {
   state = {
     isOpen: false
-  }
+  };
 
   toggle = () => {
     this.setState({ isOpen: !this.state.isOpen });
-  }
+  };
 
   render() {
     return (
       <div>
         <Navbar color="dark" dark expand="sm" className="mb-5">
           <Container>
-            <NavbarBrand href="/">
-              Shopping List
-            </NavbarBrand>
+            <NavbarBrand href="/">Shopping List</NavbarBrand>
             <NavbarToggler onClick={this.toggle} />
             <Collapse isOpen={this.state.isOpen} navbar>
               <Nav className="ml-auto" navbar>
                 <NavItem>
-                  <NavLink href="https://github.com/darlose04">Github</NavLink>
+                  <RegisterModal />
                 </NavItem>
               </Nav>
             </Collapse>
           </Container>
         </Navbar>
       </div>
-    )
+    );
   }
 }
 
