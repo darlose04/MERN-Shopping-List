e9fc4217bdacf9138fc933d6a7308711fafc337f
Zach Smith <zsmith126028@gmail.com>
2019-06-04 14:44:26 -0500

    import errorActions to call in axios catch

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { returnErrors } from "./errorActions";
 
 import {
   USER_LOADED,
@@ -40,6 +41,7 @@ export const loadUser = () => (dispatch, getState) => {
       })
     )
     .catch(err => {
+      dispatch(returnErrors(err.response.data, err.response, status));
       dispatch({
         type: AUTH_ERROR
       });
