3eab5672ebd34a9a5f07ca92b169ffa9e64c9558
Zach Smith <zsmith126028@gmail.com>
2019-06-04 14:57:27 -0500

    reformat to react component class

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,35 @@
-import React from 'react';
+import React, { Component } from "react";
 
-import { Provider } from 'react-redux';
-import store from './store';
+import { Provider } from "react-redux";
+import store from "./store";
+import { loadUser } from "./actions/authActions";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-import AppNavbar from './components/AppNavbar';
-import ShoppingList from './components/ShoppingList';
-import ItemModal from './components/ItemModal';
-import { Container } from 'reactstrap';
+import AppNavbar from "./components/AppNavbar";
+import ShoppingList from "./components/ShoppingList";
+import ItemModal from "./components/ItemModal";
+import { Container } from "reactstrap";
 
-function App() {
-  return (
-    <Provider store={store}>
-      <div className="App">
-        <AppNavbar />
-        <Container>
-          <ItemModal />
-          <ShoppingList />
-        </Container>
-      </div>
-    </Provider>
-  );
+class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <Container>
+            <ItemModal />
+            <ShoppingList />
+          </Container>
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
