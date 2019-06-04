15f7b8c458580d1b885ef25b3e024942d28e29b9
Zach Smith <zsmith126028@gmail.com>
2019-06-04 16:16:04 -0500

    fix error with reactstrap import

--- client/src/components/auth/Logout.js
+++ client/src/components/auth/Logout.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
-import { NavLink } from "react-strap";
+import { NavLink } from "reactstrap";
 import { logout } from "../../actions/authActions";
 import PropTypes from "prop-types";
 
