6be90f2cea4d1e709fc03ccdab751a2ceddcbab8
Zach Smith <zsmith126028@gmail.com>
2019-06-04 14:40:20 -0500

    create auth actions

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+import {
+  USER_LOADED,
+  USER_LOADING,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT_SUCCESS,
+  REGISTER_SUCCESS,
+  REGISTER_FAIL
+} from "./types";
+
+// Check token and load user
+export const loadUser = () => (dispatch, getState) => {
+  // User Loading
+  dispatch({ type: USER_LOADING });
+
+  // Get token from localStorage
+  const token = getState().auth.token;
+
+  // Headers
+  const config = {
+    headers: {
+      "Content-type": "application/json"
+    }
+  };
+
+  // If token, then add to headers
+  if (token) {
+    config.headers["x-auth-token"] = token;
+  }
+
+  axios
+    .get("/api/auth/user", config)
+    .then(res =>
+      dispatch({
+        type: USER_LOADED,
+        payload: res.data
+      })
+    )
+    .catch(err => {
+      dispatch({
+        type: AUTH_ERROR
+      });
+    });
+};
